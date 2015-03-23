ca5608927ca39c5859414ba9248c239768638976
jnnk <jnnknnj@gmail.com>
2015-03-23 17:54:01 +0100

    Added test utility script

---
+++ pyethapp/tests/setup_rpc_client.py
@@ -0,0 +1,35 @@
+"""Provides a simple way of testing JSON RPC commands.
+
+Intended usage:
+
+    $ ipython -i pyethapp/tests/setup_rpc_client.py
+    In [0]: call('web3_sha3', '0x')
+    Request:
+    {'id': 4,
+     'jsonrpc': '2.0',
+     'method': 'web3_sha3',
+     'params': ['0x']}
+    Reply:
+    {'id': 4,
+     'jsonrpc': '2.0',
+     'result': '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'}
+    Out[0]: u'0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
+"""
+import ast
+from pprint import pprint
+from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
+from tinyrpc.transports.http import HttpPostClientTransport
+from tinyrpc import RPCClient
+
+protocol = JSONRPCProtocol()
+transport = HttpPostClientTransport('http://127.0.0.1:5000')
+
+
+def call(method, *args, **kwargs):
+    request = protocol.create_request(method, args, kwargs)
+    reply = transport.send_message(request.serialize())
+    print "Request:"
+    pprint(ast.literal_eval(request.serialize()), width=1)
+    print "Reply:"
+    pprint(ast.literal_eval(reply), width=1)
+    return protocol.parse_reply(reply).result
