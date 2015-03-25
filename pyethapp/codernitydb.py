14d7270e0d6ffa64177d1a3284dfae1b33099bde
jnnk <jnnknnj@gmail.com>
2015-03-25 18:35:39 +0100

    Converted db classes to services

--- pyethapp/codernitydb.py
+++ pyethapp/codernitydb.py
@@ -1,158 +1,92 @@
-import os
-import threading
-from pyethereum import compress
 from hashlib import md5
+import os
+from pyethereu
+from CodernityDB.database import Database, DatabasePathException,  \
+                                RecordNotFound
+from CodernityDB.hash_index import HashIndex
+from pyethereum import compress
 from pyethereum.slogging import get_logger
+
+
 log = get_logger('db')
 
-databases = {}
 
-try:
-    from CodernityDB.database import Database, DatabasePathException,  \
-                                    RecordNotFound
-    from CodernityDB.hash_index import HashIndex
-except ImportError:
-    _CodernityDB = None
-else:
-    class MD5Index(HashIndex):
+class MD5Index(HashIndex):
 
-        def __init__(self, *args, **kwargs):
-            kwargs['key_format'] = '16s'
-            super(MD5Index, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        kwargs['key_format'] = '16s'
+        super(MD5Index, self).__init__(*args, **kwargs)
 
-        def make_key_value(self, data):
-            return md5(data['key']).digest(), None
+    def make_key_value(self, data):
+        return md5(data['key']).digest(), None
 
-        def make_key(self, key):
-            return md5(key).digest()
+    def make_key(self, key):
+        return md5(key).digest()
 
 
-    class _CodernityDB(object):
+class CodernityDB(BaseService):
+    """A service providing a codernity db interface."""
 
-        def __init__(self, dbfile):
-            self.dbfile = os.path.abspath(dbfile)
-            if dbfile in databases:
-                self.db = databases[dbfile]
-                assert isinstance(self.db, self.db.__class__)
-            else:
-                self.db = Database(dbfile)
-                try:
-                    self.db.open()
-                except DatabasePathException:
-                    self.db.create()
-                    self.db.add_index(MD5Index(dbfile, 'key'))
-                databases[dbfile] = self.db
-            self.uncommitted = dict()
-            self.uncommitted_lock = threading.Lock()
+    name = 'db'
 
-        def get(self, key):
-            if key in self.uncommitted:
-                if self.uncommitted[key] is None:
-                    raise KeyError("key not in db")
-                return self.uncommitted[key]
-            try:
-                value = self.db.get('key', key, with_doc=True)['doc']['value']
-            except RecordNotFound:
+    def __init__(self, app):
+        self.dbfile = os.path.abspath(app.config['db']['path'])
+
+    def _run(self):
+        self.db = Database(dbfile)
+        try:
+            log.info('opening db', path=self.dbfile)
+            self.db.open()
+        except DatabasePathException:
+            log.info('db does not exist, creating it', path=self.dbfile)
+            self.db.create()
+            self.db.add_index(MD5Index(dbfile, 'key'))
+        self.uncommitted = dict()
+
+    def stop(self):
+        # commit?
+        log.info('closing db')
+        self.db.close()
+
+    def get(self, key):
+        log.debug('getting entry', key=key)
+        if key in self.uncommitted:
+            if self.uncommitted[key] is None:
                 raise KeyError("key not in db")
-            return compress.decompress(value)
+            return self.uncommitted[key]
+        try:
+            value = self.db.get('key', key, with_doc=True)['doc']['value']
+        except RecordNotFound:
+            raise KeyError("key not in db")
+        return compress.decompress(value)
 
-        def put(self, key, value):
-            with self.uncommitted_lock:
-                self.uncommitted[key] = value
+    def put(self, key, value):
+        log.debug('putting entry', key=key, value=value)
+        self.uncommitted[key] = value
 
-        def commit(self):
-            log.debug('commit', db=self)
-            with self.uncommitted_lock:
-                for k, v in self.uncommitted.items():
-                    if v is None:
-                        doc = self.db.get('key', k, with_doc=True)['doc']
-                        self.db.delete(doc)
-                    else:
-                        self.db.insert({'key': k, 'value': compress.compress(v)})
-                self.uncommitted.clear()
-
-        def delete(self, key):
-            with self.uncommitted_lock:
-                self.uncommitted[key] = None
-
-        def __contains__(self, key):
-            try:
-                self.get(key)
-            except KeyError:
-                return False
-            return True
-
-        def __eq__(self, other):
-            return isinstance(other, self.__class__) and self.db == other.db
-
-        def __repr__(self):
-            return '<DB at %d uncommitted=%d>' % (id(self.db), len(self.uncommitted))
-
-        def delete_db(self):
-            del databases[self.dbfile]
-
-try:
-    import leveldb
-except ImportError:
-    _LevelDB = None
-else:
-    class _LevelDB(object):
-
-        def __init__(self, dbfile):
-            self.dbfile = os.path.abspath(dbfile)
-            if dbfile in databases:
-                self.db = databases[dbfile]
-                assert isinstance(self.db, leveldb.LevelDB)
+    def commit(self):
+        log.debug('committing', db=self)
+        for k, v in self.uncommitted.items():
+            if v is None:
+                doc = self.db.get('key', k, with_doc=True)['doc']
+                self.db.delete(doc)
             else:
-                self.db = leveldb.LevelDB(dbfile)
-                databases[dbfile] = self.db
-            self.uncommitted = dict()
-            self.lock = threading.Lock()
+                self.db.insert({'key': k, 'value': compress.compress(v)})
+        self.uncommitted.clear()
 
-        def get(self, key):
-            if key in self.uncommitted:
-                if self.uncommitted[key] is None:
-                    raise KeyError("key not in db")
-                return self.uncommitted[key]
-            o = compress.decompress(self.db.Get(key))
-            self.uncommitted[key] = o
-            return o
+    def delete(self, key):
+        log.debug('deleting entry', key=key)
+        self.uncommitted[key] = None
 
-        def put(self, key, value):
-            with self.lock:
-                self.uncommitted[key] = value
+    def __contains__(self, key):
+        try:
+            self.get(key)
+        except KeyError:
+            return False
+        return True
 
-        def commit(self):
-            log.debug('commit', db=self)
-            with self.lock:
-                batch = leveldb.WriteBatch()
-                for k, v in self.uncommitted.items():
-                    if v is None:
-                        batch.Delete(k)
-                    else:
-                        batch.Put(k, compress.compress(v))
-                self.db.Write(batch, sync=False)
-                self.uncommitted.clear()
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.db == other.db
 
-        def delete(self, key):
-            with self.lock:
-                self.uncommitted[key] = None
-
-        def _has_key(self, key):
-            try:
-                self.get(key)
-                return True
-            except KeyError:
-                return False
-
-        def __contains__(self, key):
-            return self._has_key(key)
-
-        def __eq__(self, other):
-            return isinstance(other, self.__class__) and self.db == other.db
-
-        def __repr__(self):
-            return '<DB at %d uncommitted=%d>' % (id(self.db), len(self.uncommitted))
-
-        def delete_db(self):
-            del databases[self.dbfile]
+    def __repr__(self):
+        return '<DB at %d uncommitted=%d>' % (id(self.db), len(self.uncommitted))
