27ea7b0b7247c874419d1bd9090fde77fecd9809
Heiko Heiko <heiko@heiko.org>
2015-04-05 21:35:16 +0200

    add and increment __version__

--- pyethapp/__init__.py
+++ pyethapp/__init__.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-
-__author__ = 'HeikoHeiko'
-__email__ = 'heiko@ethdev.com'
-__version__ = '0.1.0'
+# ############# version ##################
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+try:
+    _dist = get_distribution('pyethapp')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'pyethapp')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+# ########### endversion ##################
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = []
 
 setup(
     name='pyethapp',
-    version='0.1.0',
+    version='0.1.1',
     description="Python Ethereum Client",
     long_description=readme + '\n\n' + history,
     author="HeikoHeiko",
